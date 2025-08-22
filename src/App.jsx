fc01484afa1574274ad5994108609099121629fe
Bhuvan-A-R <bhuvan.ar0101@gmail.com>
2025-08-22 15:27:33 +0530

    Frontend Files Added. (02)

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,51 @@
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import "./App.css";
+import './App.css';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [title, setTitle] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState('');
 
-  const VITE_API_URL = import.meta.env.VITE_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const getTasks = async () => {
-    const response = await axios.get(`${VITE_API_URL}/api/tasks`);
-    setData(response.data);
+    const res = await axios.get(`${API_URL}/api/tasks`);
+    setTasks(res.data);
   };
 
   const createTask = async () => {
     if (!title) return;
-    await axios.post(`${VITE_API_URL}/api/tasks`, { title });
-    setTitle("");
+    await axios.post(`${API_URL}/api/tasks`, {title});
+    setTitle('');
     getTasks();
-  };
-
-  const updateTask = async (id, updatedTitle) => {
-    const response = await axios.put(`${VITE_API_URL}/api/tasks/${id}`, {
-      title: updatedTitle,
-    });
-    setData(data.map((task) => (task._id === id ? response.data : task)));
   };
 
   const deleteTask = async (id) => {
-    await axios.delete(`${VITE_API_URL}/api/tasks/${id}`);
+    await axios.delete(`${API_URL}/api/tasks/${id}`);
     getTasks();
-  };
+  }
 
   useEffect(() => {
     getTasks();
   }, []);
 
-  return (
-    <div className="App">
-      <h1>Task Manager</h1>
-      <input
-        type="text"
+  return(
+    <div>
+      <h2>Task Manager</h2>
+      <input 
+        type="text" 
         value={title}
+        placeholder="Enter the Task"
         onChange={(e) => setTitle(e.target.value)}
-        placeholder="Enter task title"
       />
-      <button onClick={createTask}>Add Task</button>
+      <button onClick={createTask}>Add</button>
       <ul>
-        {data.map((task) => (
+        {Array.isArray(tasks) ? tasks.map((task) => (
           <li key={task._id}>
             {task.title}
             <button onClick={() => deleteTask(task._id)}>Delete</button>
           </li>
-        ))}
+        )) : null}
       </ul>
     </div>
   );
